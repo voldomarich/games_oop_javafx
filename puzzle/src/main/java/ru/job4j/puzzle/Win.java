fcf619503c8e47688cb3de0157585579cdd0b623
voldomarich <vbekasov1987@gmail.com>
2021-09-11 13:28:35 +0300

    2. Сокобан. Тестовое задание. [#75283 #201304]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,27 +1,38 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean check(int[][] board, int row) {
-        boolean rsl = true;
-            for (int cell = 0; cell < board[row].length; cell++) {
-                int val = board[row][cell];
-                if (val != 1) {
-                    rsl = false;
-                    break;
-                }
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int cell = 0; cell < board[row].length; cell++) {
+            int val = board[row][cell];
+            if (val != 1) {
+                result = false;
+                break;
             }
-        return rsl;
+        }
+        return result;
     }
 
-    public static boolean check1(int[][] board, int cell) {
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
-            for (int row = 0; row < board[cell].length; row++) {
-                int val = board[row][cell];
-                if (val != 1) {
-                    result = false;
-                    break;
-                }
+        for (int row = 0; row < board[column].length; row++) {
+            int val = board[row][column];
+            if (val != 1) {
+                result = false;
+                break;
             }
+        }
+        return result;
+    }
+
+    public static boolean check(int[][] board) {
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                result = true;
+                break;
+            }
+        }
         return result;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.puzzle;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
