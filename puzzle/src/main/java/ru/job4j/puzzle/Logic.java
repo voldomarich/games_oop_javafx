21145b31f1f4cbd45cdcf7e4921d582dc00b674d
voldomarich <vbekasov1987@gmail.com>
2021-11-24 16:31:08 +0300

    2. Каркас шахматной доски [#792 #230128]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -6,46 +6,25 @@ import java.util.Arrays;
 
 public class Logic {
 
-    private final int size;
-    private final Figure[] figures;
+    private final Figure[] figures = new Figure[32];
     private int index = 0;
-
-    public Logic(int sz) {
-        size = sz;
-        figures = new Figure[size * size];
-    }
 
     public void add(Figure figure) {
         figures[index++] = figure;
     }
 
-    public boolean move(Cell source, Cell dest) {
-        boolean rst = false;
+    public void move(Cell source, Cell dest)
+            throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
         int index = findBy(source);
-        if (index != -1) {
-            Cell[] steps = figures[index].way(dest);
-            if (isFree(steps)) {
-                rst = true;
-                figures[index] = figures[index].copy(dest);
-            }
-        }
-        return rst;
-    }
-    public boolean isFree(Cell[] steps)  {
-        boolean result = steps.length > 0;
-        for (Cell cell : steps) {
-            if (findBy(cell) != -1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
+        Cell[] steps = figures[index].way(dest);
+        free(steps);
+        figures[index] = figures[index].copy(dest);
     }
 
-    public boolean free(Cell[] steps, Figure[] figures) throws OccupiedCellException {
+    private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Cell cell : steps) {
             for (Figure figure : figures) {
-                if (figures[index] != null && figures[index].equals(cell)) {
+                if (figure != null && figure.position().equals(cell)) {
                     throw OccupiedCellException;
                 }
             }
@@ -54,42 +33,18 @@ public class Logic {
     }
 
     public void clean() {
-        for (int position = 0; position != figures.length; position++) {
-            figures[position] = null;
-        }
+        Arrays.fill(figures, null);
         index = 0;
     }
 
-    private int findBy(Cell cell) {
-        int rst = -1;
+    private int findBy(Cell cell) throws FigureNotFoundException {
         for (int index = 0; index != figures.length; index++) {
-            if (figures[index] != null && figures[index].position().equals(cell)) {
-                rst = index;
-                break;
+            Figure figure = figures[index];
+            if (figure != null && figure.position().equals(cell)) {
+                return index;
             }
         }
-        return rst;
-    }
-
-    public boolean isWin() {
-        return Win.check(convert());
-    }
-
-    public int[][] convert() {
-        int[][] table = new int[size][size];
-        for (int row = 0; row != table.length; row++) {
-            for (int cell = 0; cell != table.length; cell++) {
-                int position = findBy(new Cell(row, cell));
-                if (position != -1 && figures[position].movable()) {
-                    table[row][cell] = 1;
-                }
-            }
-        }
-        return table;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(convert());
+        throw new FigureNotFoundException();
     }
 }
+
