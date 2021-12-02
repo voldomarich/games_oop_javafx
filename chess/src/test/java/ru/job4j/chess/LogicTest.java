64701dbe859549a425ffea78e701f901366398ec
voldomarich <vbekasov1987@gmail.com>
2021-12-02 04:16:36 +0300

    2. Каркас шахматной доски [#792 #230128]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -16,23 +16,19 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C8));
         logic.move(Cell.C8, Cell.C7);
-        assertThat(logic.move(Cell.C8, Cell.C7),
-                is("Пользователь двигает фигуру не по правилам шахмат"));
     }
 
     @Test(expected = FigureNotFoundException.class)
     public void fnfe() throws FigureNotFoundException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C8));
-        Cell rsl = logic.findBy(Cell.A2);
-        assertThat(rsl, is("Фигуры нет на клетке"));
+        logic.findBy(Cell.A2);
     }
 
     @Test(expected = OccupiedCellException.class)
     public void oce() {
         Logic logic = new Logic();
         logic.add(new PawnWhite(Cell.D7));
-        boolean rsl = logic.free(new Cell[] {Cell.D7, Cell.E6, Cell.F5, Cell.G4});
-        assertThat(rsl, is("Ячейка занята"));
+        logic.free(new Cell[] {Cell.D7, Cell.E6, Cell.F5, Cell.G4});
     }
 }
