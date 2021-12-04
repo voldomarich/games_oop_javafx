db8f76292623d1b197999ce1b598450edc0c86b4
voldomarich <vbekasov1987@gmail.com>
2021-12-04 19:16:03 +0300

    2. Каркас шахматной доски [#792 #230128]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -5,42 +5,80 @@ import ru.job4j.puzzle.firuges.Figure;
 import java.util.Arrays;
 
 public class Logic {
-    private final Figure[] figures = new Figure[32];
+    private final int size;
+    private final Figure[] figures;
     private int index = 0;
+
+    public Logic(int sz) {
+        size = sz;
+        figures = new Figure[size * size];
+    }
 
     public void add(Figure figure) {
         figures[index++] = figure;
     }
 
-    public void move(Cell source, Cell dest) {
+    public boolean move(Cell source, Cell dest) {
+        boolean rst = false;
         int index = findBy(source);
-        Cell[] steps = figures[index].way(dest);
-        free(steps);
-        figures[index] = figures[index].copy(dest);
-    }
-
-    private boolean free(Cell[] steps) {
-        for (Cell cell : steps) {
-            for (Figure figure : figures) {
-                if (figure != null && figure.position().equals(cell)) {
-                }
+        if (index != -1) {
+            Cell[] steps = figures[index].way(dest);
+            if (isFree(steps)) {
+                rst = true;
+                figures[index] = figures[index].copy(dest);
             }
         }
-        return true;
+        return rst;
+    }
+
+    public boolean isFree(Cell... cells) {
+        boolean result = cells.length > 0;
+        for (Cell cell : cells) {
+            if (findBy(cell) != -1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     public void clean() {
-        Arrays.fill(figures, null);
+        for (int position = 0; position != figures.length; position++) {
+            figures[position] = null;
+        }
         index = 0;
     }
 
     private int findBy(Cell cell) {
+        int rst = -1;
         for (int index = 0; index != figures.length; index++) {
-            Figure figure = figures[index];
-            if (figure != null && figure.position().equals(cell)) {
-                return index;
+            if (figures[index] != null && figures[index].position().equals(cell)) {
+                rst = index;
+                break;
             }
         }
-        return index;
+        return rst;
+    }
+
+    public boolean isWin() {
+        return Win.check(convert());
+    }
+
+    public int[][] convert() {
+        int[][] table = new int[size][size];
+        for (int row = 0; row != table.length; row++) {
+            for (int cell = 0; cell != table.length; cell++) {
+                int position = findBy(new Cell(row, cell));
+                if (position != -1 && figures[position].movable()) {
+                    table[row][cell] = 1;
+                }
+            }
+        }
+        return table;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(convert());
     }
 }
